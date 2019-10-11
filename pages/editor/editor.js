134d1364a2f16cca68b0524076d435c4c7689127
Leskut <O_o_Vampire@live.cn>
2019-10-12 00:48:44 +0800

    使用多倍屏方案

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,3 +1,8 @@
+const {
+  colors,
+  images
+} = require('./colors.js')
+console.log(images.length)
 // pages/editor/editor.js
 Page({
 
@@ -5,7 +10,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    test:1,
     bars: [{
       icon: 'skinfill',
       name: '背景',
@@ -16,7 +20,23 @@ Page({
     }],
     bgColor: '',
     panelName: '',
-    colors: ['#fdcdd2', '#fc8a82', '#fc5457', '#fc1f3e', '#b51f23', '#fedfb4', '#fdd084', '#fc8a49', '#dd4f1d', '#b84328', '#e4dc9d', '#ffe36d', '#f4d23c', '#f6c955', '#f1b630', '#9bbc53', '#648835', '#467139', '#345c3b', '#253a25', '#a6d3c4', '#73b2af', '#5da0a9', '#368a7a', '#086352', '#66aecc', '#628baa', '#5695c0', '#1063a1', '#223774', '#7a94b1', '#8081ad', '#656b9c', '#845196', '#564b81', '#f6bbd0', '#ec989b', '#fc81ab', '#fc4482', '#f2105a', '#fffefe', '#cccccc', '#999999', '#666666', '#000000']
+    colors,
+    frameHeight: 0,
+    materials: images
+  },
+  onLoad() {
+    this.resize()
+  },
+  // 用于重置画布大小
+  resize() {
+    const systemInfo = wx.getSystemInfoSync()
+    const query = wx.createSelectorQuery()
+    query.select('#panel').boundingClientRect()
+    query.exec(res => {
+      this.setData({
+        frameHeight: systemInfo.windowHeight - res[0].height
+      })
+    })
   },
   changeColor(e) {
     const color = e.currentTarget.dataset.value
@@ -26,14 +46,32 @@ Page({
   },
   showPanel(e) {
     console.log(e)
-    const { panelName } = e.currentTarget.dataset
+    const {
+      panelName
+    } = e.currentTarget.dataset
     console.log(panelName)
-    this.setData({ panelName, test: this.data.test + 1 })
+    this.setData({
+      panelName,
+    })
+    this.resize()
   },
   hidePanel() {
     this.setData({
       panelName: '',
-      test:this.data.test + 1
     })
+    this.resize()
+  },
+  changeMaterial(e) {
+    const {materials} = this.data
+    const {x,y,source} = e.detail
+    console.log(source)
+    console.log(x)
+    if (source !=='touch'){
+      // const { index } = e.currentTarget.dataset
+      // materials[index].x = x, materials[index].y = y
+      // this.setData({ materials })
+    }
+    
   }
+
 })
