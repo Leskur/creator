30e3d8c3ff183c02910df415ebae0d8eba16fb79
Leskut <O_o_Vampire@live.cn>
2019-10-14 01:50:23 +0800

    Update

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,8 +1,7 @@
 const {
   colors,
-  images
-} = require('./colors.js')
-console.log(images.length)
+  images,
+} = require('./resources.js')
 // pages/editor/editor.js
 Page({
 
@@ -16,60 +15,126 @@ Page({
       panel: 'color'
     }, {
       icon: 'emojiflashfill',
-      name: '贴纸'
+      name: '贴纸',
+      panel: 'image'
     }],
     bgColor: '#647f9c',
     panelName: '',
     colors,
-    frameHeight: 0,
-    materials: images
+    images,
+    frame: {
+      height: 0,
+      wdth: 0,
+      left: 0,
+      top: 0
+    },
+    currentImagePanelTab: 0,
+    materials: []
   },
   onLoad() {
+    this.systemInfo = wx.getSystemInfoSync()
+    console.log(this.systemInfo)
     this.resize()
   },
   // 用于重置画布大小
   resize() {
-    const systemInfo = wx.getSystemInfoSync()
+    const systemInfo = this.systemInfo || wx.getSystemInfoSync()
     const query = wx.createSelectorQuery()
     query.select('#panel').boundingClientRect()
+    query.select('#area').boundingClientRect()
     query.exec(res => {
+      console.log(res[1])
+      const frameHeight = systemInfo.windowHeight - res[0].height
+      const frameWidth = Math.min(frameHeight * 0.7, systemInfo.windowWidth)
+      const frameLeft = (systemInfo.windowWidth - frameWidth) / 2
       this.setData({
-        frameHeight: systemInfo.windowHeight - res[0].height
+        frame: {
+          height: systemInfo.windowHeight - res[0].height,
+          width: frameWidth,
+          left: frameLeft,
+          right: frameLeft + frameWidth,
+          top: 0
+        }
       })
+      console.log(this.data.frame)
     })
   },
   changeColor(e) {
     const color = e.currentTarget.dataset.value
-    this.setData({ bgColor: color })
+    this.setData({
+      bgColor: color
+    })
   },
   showPanel(e) {
-    console.log(e)
     const {
       panelName
     } = e.currentTarget.dataset
-    console.log(panelName)
     this.setData({
       panelName,
+    }, () => {
+      this.resize()
     })
-    this.resize()
+
   },
   hidePanel() {
     this.setData({
       panelName: '',
+    }, () => {
+      this.resize()
     })
-    this.resize()
+  },
+  selectPanel(e) {
+    console.log(e)
+    this.setData({
+      currentImagePanelTab: e.currentTarget.dataset.index
+    })
+  },
+  addMeterial(e) {
+    const {
+      group,
+      index
+    } = e.currentTarget.dataset
+    const {
+      materials,
+      images,
+      frame
+    } = this.data
+    const systemInfo = this.systemInfo || wx.getSystemInfoSync()
+    console.log(frame)
+    const image = images[group].list[index]
+    materials.push({
+      image,
+      width: 64,
+      height: 90,
+      left: systemInfo.windowWidth + frame.left + frame.width * 0.5,
+      top: systemInfo.windowHeight
+    })
+    this.setData({
+      materials
+    })
+    const query = wx.createSelectorQuery()
+    query.select('#material-0').boundingClientRect()
+    query.exec(res => {
+      console.log(res)
+    })
   },
   changeMaterial(e) {
-    const {materials} = this.data
-    const {x,y,source} = e.detail
-    console.log(source)
-    console.log(x)
-    if (source !=='touch'){
+    const {
+      materials
+    } = this.data
+    const {
+      x,
+      y,
+      source
+    } = e.detail
+    const index = e.currentTarget.dataset.index
+      materials[index].left = x
+      materials[index].top = y
+      this.setData({ materials })
       // const { index } = e.currentTarget.dataset
       // materials[index].x = x, materials[index].y = y
       // this.setData({ materials })
-    }
-    
+
   }
 
 })
---
+++ pages/editor/resources.js
@@ -0,0 +1,21 @@
+module.exports = {
+  colors: ['#fdcdd2', '#fc8a82', '#fc5457', '#fc1f3e', '#b51f23', '#fedfb4', '#fdd084', '#fc8a49', '#dd4f1d', '#b84328', '#e4dc9d', '#ffe36d', '#f4d23c', '#f6c955', '#f1b630', '#9bbc53', '#648835', '#467139', '#345c3b', '#253a25', '#a6d3c4', '#73b2af', '#5da0a9', '#368a7a', '#086352', '#66aecc', '#628baa', '#5695c0', '#1063a1', '#223774', '#7a94b1', '#8081ad', '#656b9c', '#845196', '#564b81', '#f6bbd0', '#ec989b', '#fc81ab', '#fc4482', '#f2105a', '#fffefe', '#cccccc', '#999999', '#666666', '#000000'],
+  images: [{
+      image: 'http://qiniu.scdztlzx.com/creator/1-2.png',
+      list: ['http://qiniu.scdztlzx.com/creator/1-1.png', 'http://qiniu.scdztlzx.com/creator/1-2.png', 'http://qiniu.scdztlzx.com/creator/1-3.png', 'http://qiniu.scdztlzx.com/creator/1-4.png', 'http://qiniu.scdztlzx.com/creator/1-5.png', 'http://qiniu.scdztlzx.com/creator/1-6.png', 'http://qiniu.scdztlzx.com/creator/1-7.png', 'http://qiniu.scdztlzx.com/creator/1-8.png', 'http://qiniu.scdztlzx.com/creator/1-9.png', 'http://qiniu.scdztlzx.com/creator/1-10.png', 'http://qiniu.scdztlzx.com/creator/1-11.png', 'http://qiniu.scdztlzx.com/creator/1-12.png', 'http://qiniu.scdztlzx.com/creator/1-13.png', 'http://qiniu.scdztlzx.com/creator/1-14.png', 'http://qiniu.scdztlzx.com/creator/1-15.png', 'http://qiniu.scdztlzx.com/creator/1-16.png', 'http://qiniu.scdztlzx.com/creator/1-17.png', 'http://qiniu.scdztlzx.com/creator/1-18.png']
+    },
+    {
+      image: 'http://qiniu.scdztlzx.com/creator/2-2.png',
+      list: ['http://qiniu.scdztlzx.com/creator/2-1.png', 'http://qiniu.scdztlzx.com/creator/2-2.png', 'http://qiniu.scdztlzx.com/creator/2-3.png', 'http://qiniu.scdztlzx.com/creator/2-4.png', 'http://qiniu.scdztlzx.com/creator/2-5.png', 'http://qiniu.scdztlzx.com/creator/2-6.png', 'http://qiniu.scdztlzx.com/creator/2-7.png', 'http://qiniu.scdztlzx.com/creator/2-8.png', 'http://qiniu.scdztlzx.com/creator/2-9.png', 'http://qiniu.scdztlzx.com/creator/2-10.png', 'http://qiniu.scdztlzx.com/creator/2-11.png', 'http://qiniu.scdztlzx.com/creator/2-12.png', 'http://qiniu.scdztlzx.com/creator/2-13.png', 'http://qiniu.scdztlzx.com/creator/2-14.png', 'http://qiniu.scdztlzx.com/creator/2-15.png', 'http://qiniu.scdztlzx.com/creator/2-16.png', 'http://qiniu.scdztlzx.com/creator/2-17.png', 'http://qiniu.scdztlzx.com/creator/2-18.png']
+    },
+    {
+      image: 'http://qiniu.scdztlzx.com/creator/3-5.png',
+      list: ['http://qiniu.scdztlzx.com/creator/3-1.png', 'http://qiniu.scdztlzx.com/creator/3-2.png', 'http://qiniu.scdztlzx.com/creator/3-3.png', 'http://qiniu.scdztlzx.com/creator/3-4.png', 'http://qiniu.scdztlzx.com/creator/3-5.png', 'http://qiniu.scdztlzx.com/creator/3-6.png', 'http://qiniu.scdztlzx.com/creator/3-7.png', 'http://qiniu.scdztlzx.com/creator/3-8.png', 'http://qiniu.scdztlzx.com/creator/3-9.png', 'http://qiniu.scdztlzx.com/creator/3-10.png', 'http://qiniu.scdztlzx.com/creator/3-11.png', 'http://qiniu.scdztlzx.com/creator/3-12.png', 'http://qiniu.scdztlzx.com/creator/3-13.png', 'http://qiniu.scdztlzx.com/creator/3-14.png', 'http://qiniu.scdztlzx.com/creator/3-15.png', 'http://qiniu.scdztlzx.com/creator/3-16.png', 'http://qiniu.scdztlzx.com/creator/3-17.png', 'http://qiniu.scdztlzx.com/creator/3-18.png']
+    },
+    {
+      image: 'http://qiniu.scdztlzx.com/creator/4-6.png',
+      list: ['http://qiniu.scdztlzx.com/creator/4-1.png', 'http://qiniu.scdztlzx.com/creator/4-2.png', 'http://qiniu.scdztlzx.com/creator/4-3.png', 'http://qiniu.scdztlzx.com/creator/4-4.png', 'http://qiniu.scdztlzx.com/creator/4-5.png', 'http://qiniu.scdztlzx.com/creator/4-6.png', 'http://qiniu.scdztlzx.com/creator/4-7.png', 'http://qiniu.scdztlzx.com/creator/4-8.png', 'http://qiniu.scdztlzx.com/creator/4-9.png', 'http://qiniu.scdztlzx.com/creator/4-10.png', 'http://qiniu.scdztlzx.com/creator/4-11.png', 'http://qiniu.scdztlzx.com/creator/4-12.png', 'http://qiniu.scdztlzx.com/creator/4-13.png', 'http://qiniu.scdztlzx.com/creator/4-14.png', 'http://qiniu.scdztlzx.com/creator/4-15.png', 'http://qiniu.scdztlzx.com/creator/4-16.png', 'http://qiniu.scdztlzx.com/creator/4-17.png', 'http://qiniu.scdztlzx.com/creator/4-18.png']
+    }
+  ],
+
+}
